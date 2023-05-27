7195f2c41c176c7567537079301a95ba3d4a2860
Seongrok Kim <seongrok.kim@wesang.com>
2023-05-27 15:25:38 +0900

    thread basic

---
+++ parallel/py_ad_1_1.py
@@ -0,0 +1,28 @@
+import logging
+import threading
+import time
+
+def thread_func(name):
+    logging.info("Sub Thread %s: starts", name)
+    time.sleep(3)
+    logging.info("Sub Thread %s: end", name)
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(
+        format=format,
+        level=logging.INFO,
+        datefmt="%H:%M:%S",
+    )
+    logging.info("Main Thread: before creating thread")
+
+    x = threading.Thread(target=thread_func, args=("First",))
+
+    x.start()
+
+    # Wait for sub thread
+    # x.join()
+
+    logging.info("Main Thread: wait for the thread to finish")
+
+    logging.info("Main Thread: all done")
